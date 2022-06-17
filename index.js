9ba731ce4135efe26f3d986cdda7e2a18ce23b96
Kanji <39612139+Slanderrr@users.noreply.github.com>
2022-06-16 21:14:33 -0600

    alert test

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function pwnMe() {
 				location.reload();
 			}
 		} else {
-      alert("Uh-oh!\n\nYou are on a desktop environment, which is not supported.\n\nUse this on a compatible iOS device.");
+      iosAlert('Uh-oh!\n\nYou are on a desktop environment, which is not supported.\n\nUse this on a compatible iOS device.', 'Etcetera');
 			location.reload();
 		}
   }
@@ -207,6 +207,34 @@ function kickstart146() {
     wa.port.postMessage("pwn");
   });
   document.getElementById("jbButton").innerHTML = "Exploit Finished! 🤪";
+}
+
+(function(proxied) {
+  window.alert = function() {
+    iosAlert(arguments[0], arguments[1]);
+  }
+})(window.alert);
+
+function iosAlert() {
+try {
+  var $alert = document.querySelector('.alert');
+  $alert.parentElement.removeChild($alert);
+} catch ($error) {}
+
+var $alert = document.createElement('span');
+if (arguments[1] == null) {
+  arguments[1] = window.location.protocol + '//' + window.location.hostname;
+}
+$alert.innerHTML = '<div class="alert"><div class="inner"><div class="title">' + arguments[1] + '</div><div class="text">' + arguments[0] + '</div></div><div class="button">OK</div></div>';
+document.querySelector('body').appendChild($alert);
+setTimeout(function() {
+  document.querySelector('.alert .button:last-child').addEventListener("click", function() {
+
+    $alert.parentElement.removeChild($alert);
+  });
+});
+return false;
+
 }
 
 const appHeight = () => {
