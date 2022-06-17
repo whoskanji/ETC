28501c0779d5882229d5d8a6e1562617f74c1dc3
Kanji <39612139+Slanderrr@users.noreply.github.com>
2022-06-16 21:57:34 -0600

    failed test. removing that shit

--- index.js
+++ index.js
@@ -254,33 +254,6 @@ function iosAlertError() {
   return false;
 }
 
-function Socials() {
-  let twitter = "Kanji's Twitter";
-  let result = twitter.link("https://twitter.com/kanjishere");
-
-  iosAlertSocials(result, 'Twitter');
-}
-
-function iosAlertSocials() {
-  try {
-    var $alert = document.querySelector('.alert');
-    $alert.parentElement.removeChild($alert);
-  } catch ($error) {}
-  
-  var $alert = document.createElement('span');
-  if (arguments[1] == null) {
-    arguments[1] = window.location.protocol + '//' + window.location.hostname;
-  }
-  $alert.innerHTML = '<div class="alert"><div class="inner"><div class="title">' + arguments[1] + '</div><div class="text">' + arguments[0] + '</div></div><div class="button">OK</div></div>';
-  document.querySelector('body').appendChild($alert);
-  setTimeout(function() {
-    document.querySelector('.alert .button:last-child').addEventListener("click", function() {
-      $alert.parentElement.removeChild($alert);
-    });
-  });
-  return false;
-}
-
 const appHeight = () => {
 	const doc = document.documentElement;
 	doc.style.setProperty("--app-height", `${window.innerHeight}px`);
