0238e8de62ff81695388020e3c75f471b729e4cf
Turnerhackz1 <turner881189@gmail.com>
2022-06-19 14:07:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ async function pwnMe() {
         iosAlert("Uh-oh!\n\niOS " + currentFirmware(navigator.userAgent) + " is not supported.\n\nPlease use an iOS 14.5 or 14.6 device.");
 			}
 		} else {
-      iosAlert('Uh-oh!\n\nYou are on a desktop environment, which is not supported.\n\nUse this on a compatible iOS device.', 'Etcetera');
+      //iosAlert('Uh-oh!\n\nYou are on a desktop environment, which is not supported.\n\nUse this on a compatible iOS device.', 'Etcetera');
+			document.getElementById("jbButton").innerHTML = "Jailbreaking 🔓📱...";
 		}
   }
 }
